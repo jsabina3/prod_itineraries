e8eca7cb72aa64858a0816d7842b96afee7e8b9d
jsabina3 <160482018+jsabina3@users.noreply.github.com>
2025-04-25 17:08:34 +0200

    Update crew.py

--- src/itineraries_final/crew.py
+++ src/itineraries_final/crew.py
@@ -104,7 +104,7 @@ class ItinerariesFinal():
             verbose=True,
             allow_delegation=False,
             async_execution=False,
-            llm = ChatOpenAI(model = 'o4-mini')
+            llm = ChatOpenAI(model = 'o4-mini', reasoning_effort = 'high')
         )
 
     @task
@@ -138,7 +138,7 @@ class ItinerariesFinal():
     def Itinerary_Curation(self) -> Task:
         return Task(
             config=self.tasks_config['Itinerary_Curation'],
-            llm = ChatOpenAI(model = 'o4-mini')
+            llm = ChatOpenAI(model = 'o4-mini', reasoning_effort = 'high')
         )
 
     @crew
